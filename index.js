130eecf815e6a3f916aaf864213feb55437b767b
Mark Edward Ratio <markedwardratio@gmail.com>
2022-09-23 20:08:52 +0800

    Add prompt that ask for number of squares per side

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const dom = {
 	grid: document.querySelector('#grid'),
+	boxesPromptBtn: document.querySelector('#boxes-prompt-btn'),
 };
 
 function handleGridSquareMouseover(event) {
@@ -18,5 +19,26 @@ function populateGrid(numOfSquarePerSide) {
 		grid.appendChild(domGridSquare);
 	}
 }
+
+function getNumOfSquarePerSide() {
+	while (true) {
+		const input = prompt('Input a number of boxes from 16 to 100.');
+		if (input === null) {
+			return null;
+		} else if (Number.isInteger(Number(input)) && input > 15 && input < 101) {
+			return Number(input);
+		}
+	}
+}
+
+function handleBoxesPromptBtn() {
+	const numOfSquarePerSide = getNumOfSquarePerSide();
+	if (numOfSquarePerSide !== null) {
+		dom.grid.innerHTML = '';
+		populateGrid(numOfSquarePerSide);
+	}
+}
+
+dom.boxesPromptBtn.addEventListener('click', handleBoxesPromptBtn);
 
 populateGrid(16);
