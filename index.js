bd7421d751f6487f61ef62a326d7d514da063985
Mark Edward Ratio <markedwardratio@gmail.com>
2022-09-23 17:16:41 +0800

    Add a 16x16 grid of square divs

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('test');
+'use strict';
+
+const dom = {
+	grid: document.querySelector('#grid'),
+};
+
+function populateGrid(numOfSquarePerSide) {
+	dom.grid.style.gridTemplate = `repeat(${numOfSquarePerSide}, 1fr) / repeat(${numOfSquarePerSide}, 1fr)`;
+	const numOfSquares = numOfSquarePerSide * numOfSquarePerSide;
+	for (let i = 0; i < numOfSquares; i++) {
+		const domGridSquare = document.createElement('div');
+		domGridSquare.classList.add('grid-square');
+		grid.appendChild(domGridSquare);
+	}
+}
+
+populateGrid(16);
