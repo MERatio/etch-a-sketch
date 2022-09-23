2d2cb59c9b5dba71a0e56569cde264c83f49ea9a
Mark Edward Ratio <markedwardratio@gmail.com>
2022-09-23 17:27:40 +0800

    Add grid box hover effect like a pen would

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const dom = {
 	grid: document.querySelector('#grid'),
 };
 
+function handleGridSquareMouseover(event) {
+	event.target.classList.add('painted');
+}
+
 function populateGrid(numOfSquarePerSide) {
 	dom.grid.style.gridTemplate = `repeat(${numOfSquarePerSide}, 1fr) / repeat(${numOfSquarePerSide}, 1fr)`;
 	const numOfSquares = numOfSquarePerSide * numOfSquarePerSide;
 	for (let i = 0; i < numOfSquares; i++) {
 		const domGridSquare = document.createElement('div');
 		domGridSquare.classList.add('grid-square');
+		domGridSquare.addEventListener('mouseover', handleGridSquareMouseover);
 		grid.appendChild(domGridSquare);
 	}
 }
