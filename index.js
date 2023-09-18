eddb819f7a425202aa8cf94b5c36e167c72f5cfc
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-18 21:54:39 +0800

    Add a btn that prompts for num of squares per side

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 'use strict';
 
+const changeSquaresPerSide = document.querySelector('.change-squares-per-side');
 const squaresContainer = document.querySelector('.squares-container');
+let squares;
+
+function getSquaresPerSide() {
+	let squaresPerSide;
+	do {
+		squaresPerSide = prompt(
+			'Input number of squares per side (minimum = 1, maximum = 100)',
+			16,
+		);
+		if (squaresPerSide === null) {
+			return null;
+		}
+		squaresPerSide = Number(squaresPerSide);
+	} while (
+		Number.isNaN(squaresPerSide) ||
+		squaresPerSide < 1 ||
+		squaresPerSide > 100
+	);
+
+	return squaresPerSide;
+}
 
 function addSquares(numOfSquares) {
 	for (let i = 0; i < numOfSquares; i++) {
@@ -12,7 +34,7 @@ function addSquares(numOfSquares) {
 }
 
 function addSquaresHoverEffect() {
-	const squares = document.querySelectorAll('.square');
+	squares = document.querySelectorAll('.square');
 	squares.forEach((square) => {
 		square.addEventListener('mouseover', (e) => {
 			e.currentTarget.style.backgroundColor = 'blue';
@@ -20,5 +42,29 @@ function addSquaresHoverEffect() {
 	});
 }
 
-addSquares(Math.pow(16, 2));
-addSquaresHoverEffect();
+function setup(firstSetup, squaresPerSide) {
+	if (!firstSetup) {
+		removeSquares();
+	}
+	addSquares(Math.pow(squaresPerSide, 2));
+	addSquaresHoverEffect();
+}
+
+function handleChangeSquaresPerSideClick() {
+	const squaresPerSide = getSquaresPerSide();
+	if (squaresPerSide === null) {
+		return;
+	}
+	setup(false, squaresPerSide);
+}
+
+function removeSquares() {
+	squares.forEach((square) => {
+		square.removeEventListener('mouseover', handleChangeSquaresPerSideClick);
+		square.remove();
+	});
+}
+
+changeSquaresPerSide.addEventListener('click', handleChangeSquaresPerSideClick);
+
+setup(true, 16);
