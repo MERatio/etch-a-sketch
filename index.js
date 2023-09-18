5c479f8672b2658dfeafdb72c114ed46732f533a
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-09-18 21:06:31 +0800

    Add bg color on square hover

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 const GRID_SIZE = Math.pow(16, 2);
 
-const squares = document.querySelector('.squares');
+const squaresContainer = document.querySelector('.squares-container');
 
 for (let i = 0; i < GRID_SIZE; i++) {
 	const square = document.createElement('div');
 	square.classList.add('square');
 	square.setAttribute('style', `width: ${100 / Math.sqrt(GRID_SIZE)}%`);
-	squares.appendChild(square);
+	squaresContainer.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+	square.addEventListener('mouseover', (e) => {
+		e.currentTarget.style.backgroundColor = 'blue';
+	});
+});
